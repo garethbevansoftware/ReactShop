34505590fe6cd8b9cb6f15f5e4e78c54334427d3
Gareth Bevan <garethbevansoftware@gmail.com>
2020-10-12 10:08:35 +0100

    Converted Shop items to cards

--- VeygoTechnicalTest/src/components/shop/ShopPageItems.js
+++ VeygoTechnicalTest/src/components/shop/ShopPageItems.js
@@ -2,33 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ShopPageItems = ({ items, handleAddToCart }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Price</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      {items.map((item) => {
-        return (
-          <tr key={item.id}>
-            <td>{item.name}</td>
-            <td>£{item.price}</td>
-            <td>
-              <button
-                className="btn btn-dark"
-                onClick={() => handleAddToCart(item)}
-              >
-                Add To Cart
-              </button>
-            </td>
-          </tr>
-        );
-      })}
-    </tbody>
-  </table>
+  <div className="card-deck">
+    {items.map((item) => {
+      return (
+        <div className="card" key={item.id}>
+          <div className="card-body">
+            <h5 className="card-title">{item.name}</h5>
+            <p className="card-text">{item.price}</p>
+            <button
+              className="btn btn-dark"
+              onClick={() => handleAddToCart(item)}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      );
+    })}
+  </div>
 );
 
 ShopPageItems.propTypes = {
